802ca961f39ce2b14022f4cb372b672a6dfcc2dd
Eliza <gluhova.liza@gmail.com>
2021-03-25 17:12:39 +0300

    home

--- home.php
+++ home.php
@@ -8,7 +8,7 @@ get_header(); ?>
 $q_home = new WP_Query( array( 'page_id' => 10 ) );
 
  if ($q_home -> have_posts() ) : while ( $q_home -> have_posts() ) : $q_home -> the_post(); ?>
-<div class="container-fluid"><?php the_content(); ?></div>
+<div class="container-fluid home-container"><?php the_content(); ?></div>
 
 <?php endwhile; ?>
 <?php endif; ?>
