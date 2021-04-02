7d9c6e488db54bdd5a1eda707933760af39707e1
Andrey-Glukhov <agi2000@mail.ru>
2021-04-02 16:52:18 +0300

    change single product

--- functions.php
+++ functions.php
@@ -33,19 +33,18 @@ function bv_theme_setup(){
   register_nav_menu('primary', 'Primary Header Navigation');
   register_nav_menu('footer_menu', 'Footer Navigation Area');
 }
-function mytheme_add_woocommerce_support() {
-    add_theme_support( 'woocommerce' );
-}
-add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
-
-
 add_action('init', 'bv_theme_setup');
+
 add_theme_support('custom-background');
 add_theme_support('custom-header');
 add_theme_support('post-formats', array('aside', 'chat', 'gallery','link','image','quote','status','video'));
 add_theme_support('post-thumbnails');
 
-
+//Add woocommerce support
+function mytheme_add_woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
 //add_action( 'wp_enqueue_scripts', 'enqueue_font_awesome' );
 
@@ -187,5 +186,54 @@ function bv_image_on_checkout( $name, $cart_item, $cart_item_key ) {
     /* Prepend image to name and return it */
     return $image . $name;
 
+}
+
+//
+remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_images', 20 );
+add_action( 'woocommerce_before_single_product_summary', 'bv_show_product_image', 20 );
+
+function bv_show_product_image()  {
+	
+	if ( ! function_exists( 'wc_get_gallery_image_html' ) ) {
+		return;
+	}
+	
+	global $product;
+	
+	$columns           = apply_filters( 'woocommerce_product_thumbnails_columns', 4 );
+	$post_thumbnail_id = $product->get_image_id();
+	$wrapper_classes   = apply_filters(
+		'woocommerce_single_product_image_gallery_classes',
+		array(
+			'woocommerce-product-gallery',
+			'woocommerce-product-gallery--' . ( $product->get_image_id() ? 'with-images' : 'without-images' ),
+			'woocommerce-product-gallery--columns-' . absint( $columns ),
+			'images',
+		)
+	);
+	?>
+	<div class=" col-md-7 col-sm-10 col-11 <?php echo esc_attr( implode( ' ', array_map( 'sanitize_html_class', $wrapper_classes ) ) ); ?>" data-columns="<?php echo esc_attr( $columns ); ?>" style="opacity: 0; transition: opacity .25s ease-in-out;">
+		<figure class="woocommerce-product-gallery__wrapper">
+			<?php
+
+			$attachment_ids = $product->get_gallery_image_ids();
+			
+			if ( $attachment_ids && $product->get_image_id() ) {
+				//foreach ( $attachment_ids as $attachment_id ) {
+					echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', wc_get_gallery_image_html( $attachment_ids[0] ,true), $attachment_ids[0] ); // phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
+				//}
+			} else {
+				$html  = '<div class="woocommerce-product-gallery__image--placeholder">';
+				$html .= sprintf( '<img src="%s" alt="%s" class="wp-post-image" />', esc_url( wc_placeholder_img_src( 'woocommerce_single' ) ), esc_html__( 'Awaiting product image', 'woocommerce' ) );
+				$html .= '</div>';
+			}
+	
+			echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', $html, $post_thumbnail_id ); // phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
+	
+			//do_action( 'woocommerce_product_thumbnails' );
+			?>
+		</figure>
+	</div>
+<?php	
 }
 ?>
--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -28,7 +28,7 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 }
 
 ?>
-<div class="container">
+<div class="container checkout-container">
 	<form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
 
 		<?php if ( $checkout->get_checkout_fields() ) : ?>
@@ -36,7 +36,7 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 			<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 
 			<div class="col2-set  row" id="customer_details">
-				<div class="col-md-6">
+				<div class="col-md-6 col-sm-12">
 					<?php do_action( 'woocommerce_checkout_billing' ); ?>
 					<div class="col-2">
 						<?php do_action( 'woocommerce_checkout_shipping' ); ?>
@@ -44,18 +44,18 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 				</div>
 			<?php //do_action( 'woocommerce_checkout_after_customer_details' ); ?>
 
-		<?php endif; ?>
-		<div class="col-md-6">	
-			<?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
+			<?php endif; ?>
+			<div class="col-md-6 col-sm-12">	
+				<?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
 
-			<h3 id="order_review_heading"><?php esc_html_e( 'Cart details', 'woocommerce' ); ?></h3>
+				<h3 id="order_review_heading"><?php esc_html_e( 'Cart details', 'woocommerce' ); ?></h3>
 
-			<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
+				<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
 
-			<div id="order_review" class="woocommerce-checkout-review-order">
-				<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+				<div id="order_review" class="woocommerce-checkout-review-order">
+					<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+				</div>
 			</div>
-		</div>
 		<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
 
 	</form>
